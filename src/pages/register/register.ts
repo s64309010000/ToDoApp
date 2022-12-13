775457efb93ff89537c093b7ac7138d33ca7f006
s64309010000 <s64309010000@ptnicec.ac.th>
2022-12-13 17:25:20 +0700

    edit register

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Loading, LoadingController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator } from '../../validators/email';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { LoginPage } from '../login/login';
 
 
 @IonicPage()
@@ -11,10 +15,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterPage {
 
   public signupForm: FormGroup;
+  public loading: Loading;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              public formBuiler: FormBuilder
+              public formBuiler: FormBuilder,
+              public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController,
+              public afAuth: AngularFireAuth,
+              public firestore: AngularFirestore
               ) {
 
         this.signupForm = formBuiler.group({
@@ -28,6 +37,42 @@ export class RegisterPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  signupUser() {
+    if (this.signupForm.value.password == this.signupForm.value.retype) {
+      this.afAuth.auth.createUserWithEmailAndPassword(this.signupForm.value.email, this.signupForm.value.password)
+        .then(() => {
+          let userId = this.afAuth.auth.currentUser.uid;
+          let userDoc = this.firestore.doc<any>('users/' + userId);
+          userDoc.set({
+            firstName: this.signupForm.value.firstName,
+            lastName: this.signupForm.value.lastName,
+            email: this.signupForm.value.email
+          });
+          this.navCtrl.setRoot(LoginPage);
+        }, (error) => {
+          this.loading.dismiss().then(() => {
+            let alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: "Ok", role: 'cancel' }]
+            });
+            alert.present();
+          });
+        });
+  
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+        content: "Signing up.."
+      });
+      this.loading.present();
+    } else {
+      let alert = this.alertCtrl.create({
+        message: "The passwords do not match.",
+        buttons: [{ text: "Ok", role: 'cancel' }]
+      });
+      alert.present();
+    }
   }
 
 }
